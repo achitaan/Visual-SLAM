60f008dafacbcfb7ba3bb2443fb90b94c7a016d0
Achita <ssgssachita@example.com>
2024-12-20 01:08:23 -0500

    Updated Visual Odometry

--- src/VisualOdometry.py
+++ src/VisualOdometry.py
@@ -2,22 +2,24 @@ import os
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt 
+from numpy.typing import NDArray
+
 class VisualOdometry:
-    def __init__(self, calibration_path):
+    def __init__(self, folder_path, calibration_path):
         self.K, _ = self.calc_camera_matrix(calibration_path)  # Intrinsic camera matrix (example values)
         self.true_poses = self._load_poses(r"KITTI_sequence_2\poses.txt")
         self.poses = [self.true_poses[0]]  # Initial pose (identity matrix)
 
-        self.I = []  # Key frames (images)
-        self.orb = cv.ORB_create(nfeatures=1500)
+        self.I = self._load_img(folder_path) # Key frames (images)
+        self.orb = cv.ORB_create(nfeatures=3000)
 
         FLANN_INDEX_KDTREE = 0
         index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
         search_params = dict(checks=50)
         self.flann = cv.FlannBasedMatcher(index_params, search_params)
 
-    def calc_camera_matrix(self, file_path: str) -> np.ndarray:
-        with open(file_path, 'r') as f:
+    def calc_camera_matrix(self, filepath: str) -> NDArray:
+        with open(filepath, 'r') as f:
             params = np.fromstring(f.readline(), dtype=np.float64, sep=' ')
             P = np.reshape(params, (3, 4))
             K = P[0:3, 0:3]
@@ -34,16 +36,40 @@ class VisualOdometry:
                 poses.append(T)
         return poses
 
+    @staticmethod
+    def _transform(R: NDArray[np.float32], t: NDArray[np.float32]) -> NDArray[np.float32]:
+        """
+        Computes the transformation matrix T_k from R_k and t_k
 
-    def load_img(self, folder_path):
-        """Load images from the specified folder."""
-        for filename in sorted(os.listdir(folder_path)):
-            img_path = os.path.join(folder_path, filename)
-            img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
+        Parameters:
+            R (ndarray): 2D numpy array of shape (3, 3)
+            t (ndarray): 1D numpy array of shape (1,)
+
+        Returns:
+            T (ndarray): 2D numpy array of shape (4, 4)
+        """
+        T = np.eye(4, dtype=np.float64)
+        T[:3, :3] = R; T[:3, 3] = np.squeeze(t)
+        return T
+
+    @staticmethod
+    def _load_img(filepath: str) -> list[NDArray]:
+        """
+        Load images from the specified folder
+        
+        Parameters:
+            filepath (str): path to folder
+            
+        """
+        images = []
+        for filename in sorted(os.listdir(filepath)):
+            path = os.path.join(filepath, filename)
+            img = cv.imread(path, cv.IMREAD_GRAYSCALE)
             if img is not None:
-                self.I.append(img)
+                images.append(img)
+        return images
 
-    def matchFeatures(self, i):
+    def matchFeatures(self, i: int) -> tuple[NDArray, NDArray]:
         kp1, desc1 = self.orb.detectAndCompute(self.I[i - 1], None)
         kp2, desc2 = self.orb.detectAndCompute(self.I[i], None)
 
@@ -51,9 +77,6 @@ class VisualOdometry:
             desc1 = desc1.astype(np.float32)
         if desc2.dtype != np.float32:
             desc2 = desc2.astype(np.float32)
-
-        #print(f"desc1 type: {desc1.dtype}, shape: {desc1.shape}")
-        #print(f"desc2 type: {desc2.dtype}, shape: {desc2.shape}")
 
         matches = self.flann.knnMatch(desc1, desc2, k=2)
 
@@ -65,18 +88,41 @@ class VisualOdometry:
 
         p1 = np.float32([kp1[m.queryIdx].pt for m in good_matches])
         p2 = np.float32([kp2[m.trainIdx].pt for m in good_matches])
-        #print("done")
         return p1, p2
 
-    def find_pose(self, p1, p2):
+    def find_transf_fast(self, p1: NDArray, p2: NDArray) -> NDArray:
         E, mask = cv.findEssentialMat(p1, p2, self.K, method=cv.RANSAC, prob=0.999, threshold=1.0)
         _, R, t, mask = cv.recoverPose(E, p1, p2, self.K)
 
-        T = np.eye(4, dtype=np.float64)
-        T[:3, :3] = R
-        T[:3, 3] = np.squeeze(t)
+        T = self._transform(R, t)
 
-        self.poses.append(self.poses[-1] @ np.linalg.inv(T))
+        return np.linalg.inv(T) 
+
+    def find_transf(self, p1: NDArray, p2: NDArray) -> NDArray:
+        E, mask = cv.findEssentialMat(p1, p2, self.K, method=cv.RANSAC, prob=0.999, threshold=1.0)
+        R1, R2, t = cv.decomposeEssentialMat(E)
+        t = np.squeeze(t)
+
+        pairs = [(R1, t), (R1, -t), (R2, t), (R2, -t)]
+        P1 = self.K @ np.eye(3, 4)
+        
+        max_z_count, best_pose = 0, 0
+        for R, t in pairs:
+            P2 = np.concatenate((self.K, np.zeros((3, 1))), axis=1) @ self._transform(R, t)
+
+            points_4d_hom = cv.triangulatePoints(P1, P2, p1.T, p2.T)
+            p1_3d_hom = points_4d_hom[:3] / points_4d_hom[3]
+            p2_3d_hom = R @ p1_3d_hom + t.reshape(-1, 1)
+            z1, z2 = p1_3d_hom[2], p2_3d_hom[2]
+
+            pos_z_count = np.sum((z1 > 0) & (z2 > 0))
+ 
+            if pos_z_count > max_z_count:
+                max_z_count = pos_z_count
+                best_pose = (R, t)
+
+        R, t = best_pose
+        return np.linalg.inv(self._transform(R, t))
 
     def triangulation(self, p1, p2):
         P1 = self.K @ np.eye(3, 4)  # First camera projection matrix
@@ -85,8 +131,8 @@ class VisualOdometry:
         points_3d = points_4d[:3] / points_4d[3]  # Convert to 3D points
         return points_3d.T
     
-    def save_to_txt(self, file_name):
-        with open(file_name, 'w') as f:
+    def save_to_txt(self, filepath: str) -> None:
+        with open(filepath, 'w') as f:
             for i, pose in enumerate(self.poses):
                 f.write(f"Pose {i}: \n")
                 np.savetxt(f, pose, fmt="%6f")
@@ -109,21 +155,15 @@ class VisualOdometry:
         plt.grid()
         plt.show()
 
-    def main(self, folder_path):
-        self.load_img(folder_path)
-
+    def main(self):
         for i in range(1, len(self.I)):
             p1, p2 = self.matchFeatures(i)
-            #print(f"self.K shape: {self.K.shape}, dtype: {self.K.dtype}")
 
-            self.find_pose(p1, p2)
-            points_3d = self.triangulation(p1, p2)
-            #print(points_3d)
-            #print(f"self.points shape: {points_3d.shape}, dtype: {points_3d.dtype}")
+            T = self.find_transf_fast(p1, p2)
+            self.poses.append(self.poses[-1] @ T)
 
             print((self.poses[i][0, 3], self.poses[i][1, 3], self.poses[i][2, 3]))
-            # Visualization (optional): Display trajectory or 3D points
-            #print(f"Frame {i}: Estimated pose:\n{self.poses[-1]}")
+
 
         print("Visual Odometry completed.")
         self.save_to_txt("poses.txt")
@@ -132,6 +172,7 @@ class VisualOdometry:
 
 # Example usage
 if __name__ == "__main__":
-    vo = VisualOdometry(r"KITTI_sequence_2\calib.txt")
     folder_path = r"KITTI_sequence_2\image_l" 
-    vo.main(folder_path)
+
+    vo = VisualOdometry(folder_path, r"KITTI_sequence_2\calib.txt")
+    vo.main()

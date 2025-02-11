8fc90dd634184a29e08c82ec6c87b3ec2d18f84a
Achita <ssgssachita@gmail.com>
2025-02-11 03:50:50 -0500

    pls grpah

--- src/SLAM.py
+++ src/SLAM.py
@@ -1,8 +1,12 @@
-
+import g2o
 
 
 class SLAM:
     def __init__(self):
         pass
 
-    
+    def __init_graph(self):
+        self.optimizer = g2o.SparseOptimizer()
+        solver = g2o.BlockSolverSE3(g2o.LinearSolverEigenSE3())
+        solver = g2o.OptimizationAlgorithmLevenberg(solver)
+        self.optimizer.set_algorithm(solver)

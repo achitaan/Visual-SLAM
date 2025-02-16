d693c65765c6e4ede43657592d07c01d162231dd
Achita <ssgssachita@gmail.com>
2025-02-16 02:56:28 -0500

    t

--- src/__init__.py
+++ src/__init__.py
@@ -64,7 +64,7 @@ def plot3D(curr_poses, gt_poses: list[NDArray] | None = None) -> None:
 
 def main() -> None:
     folder_path = r"sequences\01\image_"  # Folder containing stereo images (subfolders for left/right)
-    vo = StereoVisualOdometry(folder_path, r"sequences\01\calib.txt", use_brute_force=True)
+    vo = StereoVisualOdometry(folder_path, r"sequences\00\calib.txt", use_brute_force=True)
     num_frames = len(vo.Images_1)
     for i in range(1, num_frames):
         T = vo.find_transf_pnp(i)
